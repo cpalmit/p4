5ba155bc9d84420c4c834071af5d30e2eca48e71
CP <cpalmit@users.noreply.github.com>
2014-12-10 00:23:08 -0500

    create production database.php file

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'socialdirectory',
+            'username'  => 'root',
+            'password'  => '11lqrRS0Aa',
+        ),
+
+    ),
+
+);
