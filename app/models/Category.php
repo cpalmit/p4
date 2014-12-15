de54cfe24761918ae58c6f87ed09b8f6f9a04a09
CP <cpalmit@users.noreply.github.com>
2014-12-14 21:18:13 -0500

    added getCategory method

--- app/models/Category.php
+++ app/models/Category.php
@@ -7,4 +7,17 @@ class Category extends Eloquent {
         //category has many accounts
         return $this->hasMany('Account');
     }
+    
+    
+    public static function getCategory() {
+		$categories = Array();
+		$collection = Category::all();
+			foreach($collection as $category) {
+				$categories[$category->id] = $category->name;
+			}
+		return $categories;
+	}
+    
+    
+    
 }
