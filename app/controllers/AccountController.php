31836efc57eeb9cc397671cd28594b937eae9afb
CP <cpalmit@users.noreply.github.com>
2014-12-18 01:38:15 -0500

    added postEdit, postDelete, and search

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -10,17 +10,40 @@ class AccountController extends BaseController {
 
 	// show homepage with all accounts
 	public function getIndex() {
-		$accounts=Account::all();
+		$accounts=Account::orderBy('name')->get();
 		
 		return View::make('index')
 			->with('accounts',$accounts);
+			
 	}
-
+	
+	// search results are posted back to index
+	public function postIndex() {
+		$query = Input::get('query');
+		
+		if($query==null) {
+			 return Redirect::action('AccountController@getIndex')
+	        	->with('flash_message', 'Please enter a search term.');
+	    } 
+	    
+	    $accounts = Account::search($query);
+		if($accounts->isEmpty()) {
+			return Redirect::action('AccountController@getIndex')
+	        	->with('flash_message', 'Sorry, no accounts found when searching for "' . $query .'".');
+		} else {
+			return View::make('index')
+				->with('accounts',$accounts)
+				->with('query',$query);
+		}
+	
+	}	
+	
 
 
 	// form to create a new account
 	public function getCreate() {
 		$categories=Category::getCategory();
+		
 		return View::make('create')
 			->with('categories',$categories);
 	}
@@ -37,27 +60,46 @@ class AccountController extends BaseController {
 		return Redirect::action('AccountController@getIndex')->with('flash_message','Your account has been added.');
 	}
 
+	// getSelect and postSelect are to get you to the edit page.
+	// form to select the item you want to edit
+	public function getSelect() {
+		$accounts=Account::getAccounts();
+		return View::make('select')
+    			->with('accounts', $accounts);
+	}
+	
+	// sends select results to getEdit
+	public function postSelect() {
+	
+		$input = Input::all();
+		$id = $input['account'];
+		//Log::info("This is the id from postSelect" . $id);
+		return Redirect::action('AccountController@getEdit', array($id));
+	}
+
 	
 	// form to edit account
 	public function getEdit($id) {
-		try {
+	
+		//Log::info("Look, it's getEdit! Here's the id " . $id);
+		$accounts=Account::getAccounts();
+
+		if (isset($id)) {
+			$account = Account::where('id','=',$id)->findorFail($id);
 			// get all the categories in case you want to change that
 			$categories=Category::getCategory();
-			
-			// get account 
-		    $account = Account::where('id','=',$id)->findorFail($id);
-		    
-			} catch(exception $e) {
-		    	return Redirect::action('AccountController@getIndex')->with('flash_message', 'Account not found');
-			}
-    	return View::make('edit')
-    		->with('account', $account)
-    		->with('categories', $categories);
+			//Log::info("made it to the if");
+			return View::make('edit')
+    			->with('account', $account)
+    			->with('accounts', $accounts)
+    			->with('categories', $categories);
+		 }	
+    	
 	}
 	
 	
 	
-	// process form to edit account- brute force for now. Ideally it only updates unchanged fields.
+	// process form to edit account 
 	public function postEdit() {
 		try {
 			$id = Input::get('id');
@@ -78,6 +120,9 @@ class AccountController extends BaseController {
 
 	}
 	
+	
+	
+	
 	// process account deletion
 	public function postDelete() {
 		
@@ -87,9 +132,11 @@ class AccountController extends BaseController {
 	    catch(exception $e) {
 	        return Redirect::action('AccountController@getIndex')->with('flash_message', 'Could not delete account - not found.');
 	    }
+	    
 	    Account::destroy(Input::get('id'));
-	    return Redirect::action('AccountController@getIndex')->with('flash_message', 'Account deleted.');
+	    	return Redirect::action('AccountController@getIndex')->with('flash_message', 'Account deleted.');
 	}
+	
 
 
 
