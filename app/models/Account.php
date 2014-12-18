02ea52a931a072a13a36cbccf1fdf3b11cf917ec
CP <cpalmit@users.noreply.github.com>
2014-12-18 01:40:11 -0500

    added search() and getAccounts()

--- app/models/Account.php
+++ app/models/Account.php
@@ -13,7 +13,61 @@ class Account extends Eloquent {
     }
     
     
+    // search accounts
+    public static function search($query) {
+        if($query) {
+          
+            $accounts = Account::with('category')
+            ->whereHas('category', function($q) use($query) {
+                $q->where('name', 'LIKE', "%$query%");
+            })
+            ->orWhere('name', 'LIKE', "%$query%")
+            ->orderBy('name')->get();
+        }
+        /* Otherwise, return null
+        else {
+            $accounts = null;
+        }*/
+        return $accounts;
+    }    
     
+    // return list of all accounts
+    public static function getAccounts() {
+		$accounts = Array();
+		$collection = Account::all();
+			foreach($collection as $account) {
+				$accounts[$account->id] = $account->name;
+			}
+		return $accounts;
+	}
+    
+    /*
+    public static function printAccount() {
+    
+        $accounts = Account::all();
+
+		# Make sure we have results before trying to print them...
+		if($accounts->isEmpty() != TRUE) {
+
+		
+			foreach($accounts as $account) {
+				$twitter = $account->twitter;
+			
+				if ($twitter) {
+					$tweet = '<a href="' . $account->twitter. '" /> <img src="images/twitter.png" height="20" width="20" /></a>';
+				} else {
+					$tweet = "";
+				}
+			
+				//echo $account->name. ' | <a href="' . $account->twitter. '" /> <img src="images/twitter.png" /></a><br>';
+				echo $account->name . ' '. $tweet . '<br>';
+			}
+		} else {
+			return 'No accounts found';
+		}
+    
+    }
+    */
     
     
 }
